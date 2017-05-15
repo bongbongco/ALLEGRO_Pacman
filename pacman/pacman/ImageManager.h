07c67075b09ba484d0f94a83c051f74cf25bad70
LeeSeungyong <bongbongco@gmail.com>
2017-05-16 07:37:00 +0900

    Add CacheImage

--- pacman/pacman/ImageManager.h
+++ pacman/pacman/ImageManager.h
@@ -2,10 +2,11 @@
 #include "allegro5\allegro.h"
 #include "allegro5\allegro_image.h"
 #include "Common.h"
+#include <map>
 
 class CImageManager {
 private:
-
+	std::map<const char*, ALLEGRO_BITMAP*> m_cacheImage;
 public:
 	ALLEGRO_BITMAP *GetImage(const char *_imageFilePath);
 
